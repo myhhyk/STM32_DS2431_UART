7c36bdca592abbee68ddf1648813a1758591454f
myhhyk <67016897+myhhyk@users.noreply.github.com>
2020-06-16 20:50:16 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,5 @@
 #include "main.h"
-
-void setupRCC(void);
-void setupPORTC(void);
+#include "Kiti.h"
 
 int main(void)
 {
@@ -12,20 +10,8 @@ int main(void)
 	  GPIOC->ODR ^=		(1<<13);
 	  for(uint16_t i = 0; i<0xffff;i++);
   }
+}
 
-}
-void setupRCC(void){
-	RCC->CR			|=	(16<<3);//HSITRIM = 16
-	RCC->CR			|= 	(1<<0);//HSI on
-
-	RCC->CFGR		|=	(5<<24);//MCO HSI sel
-	RCC->APB2ENR	|=	(1<<4);//IOPC
-}
-void setupPORTC(void){
-	GPIOC->CRH		&=	~(15<<20);//0b0000<<20
-	GPIOC->CRH		|=	 (2<<20);//mode output 2mHz
-	GPIOC->CRH 		&=	~(3<<22);//cnf  push/pull
-}
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
